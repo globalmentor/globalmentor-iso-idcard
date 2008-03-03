42e3109fc7565e979852e7c5a7e3999a437e2065
Garret Wilson <garret@globalmentor.com>
2008-03-03 04:48:26 +0000

    Renamed package com.garretwilson.csv to com.globalmentor.text.csv and added copyright and license information. Renamed package com.garretwilson.iso to com.globalmentor.iso and added copyright and license information. Renamed package com.garretwilson.itu to com.globalmentor.itu and added copyright and license information. Renamed package com.garretwilson.javascript to com.globalmentor.javascript and added copyright and license information. Renamed package com.garretwilson.jnlp to com.globalmentor.jnlp and added copyright and license information. Renamed package com.garretwilson.mail to com.globalmentor.mail and added copyright and license information. Renamed package com.garretwilson.math to com.globalmentor.math and added copyright and license information.

--- src/com/globalmentor/iso/idcard/IDCard.java
+++ src/com/globalmentor/iso/idcard/IDCard.java
@@ -1,11 +1,27 @@
-package com.garretwilson.iso.idcard;
+/*
+ * Copyright © 1996-2008 GlobalMentor, Inc. <http://www.globalmentor.com/>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.globalmentor.iso.idcard;
 
 import java.util.*;
 
 import com.globalmentor.util.*;
 
 /**An encapsulation of an ISO identity card, such as as defined in ISO/IEC 7812-1:2000(E),
-	"Identification cards � Identification of issuers � Part 1: Numbering system".
+	"Identification cards — Identification of issuers — Part 1: Numbering system".
 This implementation recognizes common types of ID cards, such as MasterCard and Visa credit cards, from the Primary Account Number (PAN).
 @author Garret Wilson
 */
--- src/com/globalmentor/iso/idcard/PAN.java
+++ src/com/globalmentor/iso/idcard/PAN.java
@@ -1,16 +1,32 @@
-package com.garretwilson.iso.idcard;
+/*
+ * Copyright © 1996-2008 GlobalMentor, Inc. <http://www.globalmentor.com/>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.globalmentor.iso.idcard;
 
 import static com.globalmentor.java.CharSequences.*;
 import static com.globalmentor.java.Objects.*;
 
-import com.garretwilson.math.Luhn;
 import static com.globalmentor.java.Characters.*;
 import com.globalmentor.java.Integers;
 import com.globalmentor.java.Objects;
+import com.globalmentor.math.Luhn;
 import com.globalmentor.text.ArgumentSyntaxException;
 
 /**Primary Account Number (PAN) of an identification card as defined in ISO/IEC 7812-1:2000(E),
-	"Identification cards � Identification of issuers � Part 1: Numbering system".
+	"Identification cards — Identification of issuers — Part 1: Numbering system".
 @see <a href="http://en.wikipedia.org/wiki/ISO_7812">Wikipedia: ISO 7812</a>
 @author Garret Wilson
 */
